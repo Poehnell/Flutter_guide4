353317b5f3b354bfeefa43a89e87b177949237e0
Ty <poehnell28@gmail.com>
2021-02-11 14:05:38 -0700

    deleted getData method and created getTime async method. Creating a Response method usoing await and get to get data from an endpoint using worldtimeapi data from the web.using the jasondecode mother to convert the data into a map. Created 2 string datetime and offset. created a DateTime method call (now) which is equal to the parsed data from the maps datetime. using the add function to (now) to add the offset data from the map. We then print (now) and it prints the current date and time to the terminal.

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,19 +9,29 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+  void getTime() async {
+    // make the request
+    Response response = await get('https://worldtimeapi.org/api/timezone/America/Edmonton');
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    //print(data);
 
+    //get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(0,3);
+    // print(datetime);
+     print(offset);
+
+    //create a dateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+    
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
