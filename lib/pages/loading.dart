a26866a92e5113c28e86414793e8871e88fc7ab9
Ty <poehnell28@gmail.com>
2021-02-12 16:27:20 -0700

    in the loading screen we changes the navigator from pudsAddNamed to pushReplacementNamed(context)/home, arguments: location, flag and time set to equal ht e world time passed in data. this will now replace the loading screen with home screen once loading screen is done. we then creaded a Map variable called data in the home screen. in the build method in the home sceen we set data to equal the route arguments datat. this now will load the data and pass it into the home scrren and then remove the the loading screen having the home screen up front with the data from the loading screen.

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -7,17 +7,20 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  String time = 'loading';
 
 void setupWorldTime() async{
 
 
   WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
   await instance.getTime();
-  print(instance.time);
-  setState(() {
-    time = instance.time;
+  Navigator.pushReplacementNamed(context, '/home', arguments: {
+    'location': instance.location,
+    'flag': instance.flag,
+    'time': instance.time,
+
+
   });
+
 }
 
   @override
@@ -31,7 +34,7 @@ void setupWorldTime() async{
     return Scaffold(
       body: Padding(
        padding: EdgeInsets.all(50.0),
-        child: Text(time),
+        child: Text('Loading...'),
       ),
     );
   }
